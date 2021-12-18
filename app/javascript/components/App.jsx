9a4ffe315f56fabc2987ea27df1adb2c05b3b185
athiraramanan <athiraramanan29@gmail.com>
2021-12-18 01:29:32 +0000

    React initial page added

---
+++ app/javascript/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Routes from "../routes/Index";
+
+const App = (props) =>{
+	return (
+		<React.Fragment>
+			{Routes}
+		</React.Fragment>
+	);
+}
+
+export default App;
